fec36a9cd4113083314421dfb281035b41710ec6
Daniel Linkov <daniel_linkov@workmail.com>
2025-09-05 23:05:05 +0300

    initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>TuneSleeve</title>
+    <link rel="stylesheet" href="/themes/vapor.min.css" class="theme">
+    <link rel="icon" type="image/x-icon" href="/images/favicon.svg">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+    <div id="app">
+    </div>
+</body>
+</html>
---
+++ vite.config.js
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vuePlugin from '@vitejs/plugin-vue';
+import svgLoader from 'vite-svg-loader';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+        vuePlugin(),
+        svgLoader({
+            svgoConfig: {
+                plugins: [
+                    // All default plugins, but override removeViewBox
+                    {
+                        name: 'preset-default',
+                        params: {
+                            overrides: {
+                                removeViewBox: false,
+                            },
+                        },
+                    },
+                ],
+            },
+        }),
+    ],
+});
